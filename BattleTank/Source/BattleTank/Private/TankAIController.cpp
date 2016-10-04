191c6f7d7572179dc1d183bc5a07bd9f1feb2a71
Gaurav Sinha <gaurav.sinha1563@gmail.com>
2016-10-04 12:48:49 -0400

    Sec 04 - Lec 116 - Creating an AI Controller Class. The common Tank Blueprint is used. The difference is who owns them. The game mode level blueprint settings lets the player controller relate to the tank that we control. All other tanks will be AI tanks. The default pawn and controller menu options lets us do that and change to the Tank as the pawn and our custom PlayerController as the controller. Logging is done to prove that this lecture works.

---
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BattleTank.h"
+#include "TankAIController.h"
+
+void ATankAIController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UE_LOG(LogTemp, Warning, TEXT("AIController Begin Play."));
+
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AIController not possessing a tank."));
+	}
+	else
+	{
+		// * for converting the returned string to TCHAR *.
+		UE_LOG(LogTemp, Warning, TEXT("AIController possessing: %s"), *(ControlledTank->GetName()));
+	}
+}
+
+ATank* ATankAIController::GetControlledTank() const
+{
+
+	return Cast<ATank>(GetPawn());
+}
+
