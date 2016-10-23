0204984163a3a02cb503bb86b04dcbad79add343
Gaurav Sinha <gaurav.sinha1563@gmail.com>
2016-10-23 15:46:13 -0500

    Sec 04 - Lec 123 - Unify Player & AI Aiming. The AI tanks now log our location. We as the player log the crosshair's location in the world through a unified API that considers which tank is calling the AimAt method and reports accordingly. The AI points at us and we point at whatever the crosshair's hit location is.

--- BattleTank/Source/BattleTank/Public/TankAIController.h
+++ BattleTank/Source/BattleTank/Public/TankAIController.h
@@ -15,10 +15,12 @@ class BATTLETANK_API ATankAIController : public AAIController
 	GENERATED_BODY()
 	
 private:
-	ATank* GetControlledTank() const;
 
 	virtual void BeginPlay() override;
+
+	virtual void Tick(float DeltaSeconds) override;
+
+	ATank* GetControlledTank() const;
 	
 	ATank* GetPlayerTank() const;
-	
 };
